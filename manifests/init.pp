fcf1c6ddc81ecf740cd63f2a7c526bb3f6a02461
root <root@mom.globomantics.loc>
2020-09-03 16:10:22 +0100

    added config class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,10 @@
 class apache (
-  String $install_name
+  String $install_name,
+  Array $config_paths
 ) {
-  include apache::install
+  contain apache::install
+  contain apache::config
+
+  Class['::apache::install']
+  -> Class['::apache::config']
 }
